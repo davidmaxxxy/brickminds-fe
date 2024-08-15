b9b362a6ee62c82715dca650079153dad8999a60
davidmaxy1994 <maksymiukdavid@gmail.com>
2024-08-15 21:40:03 +0100

    Imp 2: Implemented a draft page consisting of a header, some buttons, circles indicating the stage that we are at, footer and body for API response test

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./components/HomePage/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* Add additional routes here as needed */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Header/Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>BrickMinds</h1>
+    </header>
+  );
+};
+
+export default Header;
